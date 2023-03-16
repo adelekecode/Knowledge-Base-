692c7b44bde887a167ddf946d4a9f9d4d92c516e
Henzyd <henzydee@gmail.com>
2023-03-16 01:34:20 +0100

    FIX: access token refresh

--- Knowledge-Base-Frontend-main/src/components/ProtectRoutes/index.jsx
+++ Knowledge-Base-Frontend-main/src/components/ProtectRoutes/index.jsx
@@ -27,35 +27,37 @@ const ProtectRoutes = ({ children }) => {
     }
   });
 
-  (async function () {
-    try {
-      const accessToken = localStorage.getItem("accessToken");
-      const refreshToken = localStorage.getItem("refreshToken");
-      await axios
-        .post("/auth/token/verify", { token: accessToken })
-        .then((response) => console.log("res: /> ", response))
-        .catch(async (error) => {
-          if (error.response?.status === 401) {
-            await axios
-              .post("/auth/refresh", {
-                refresh: refreshToken,
-              })
-              .then((res) => {
-                localStorage.setItem("accessToken", res.data.access);
-              })
-              .catch((err) => {
-                console.log(err);
-                if (err.response?.status === 401) {
-                  notifyError("You token has expired, please login again");
-                  return navigate("/login");
-                }
-              });
-          }
-        });
-    } catch (error) {
-      console.log("error in try block: > ", error);
-    }
-  })();
+  setInterval(() => {
+    (async function () {
+      try {
+        const accessToken = localStorage.getItem("accessToken");
+        const refreshToken = localStorage.getItem("refreshToken");
+        await axios
+          .post("/auth/token/verify", { token: accessToken })
+          // .then((response) => console.log("res: /> ", response))
+          .catch(async (error) => {
+            if (error.response?.status === 401) {
+              await axios
+                .post("/auth/refresh", {
+                  refresh: refreshToken,
+                })
+                .then((res) => {
+                  localStorage.setItem("accessToken", res.data.access);
+                })
+                .catch((err) => {
+                  console.log(err);
+                  if (err.response?.status === 401) {
+                    notifyError("You token has expired, please login again");
+                    return navigate("/login");
+                  }
+                });
+            }
+          });
+      } catch (error) {
+        console.log("error in try block: > ", error);
+      }
+    })();
+  }, 120000);
 
   return children;
 };
